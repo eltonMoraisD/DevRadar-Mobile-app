e09c0a9b6f72e64f52277fad4ce3ade454cb05ee
eltonMoraisD <eltondelgadomorais@gmail.com>
2020-04-28 20:24:33 -0100

    adicionar um mapa a view Main

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import MapView from 'react-native-maps';
+
 function Main() {
 	return (
-		<View/>
+		<MapView style={{flex:1}}/>
 	);
 }
 
